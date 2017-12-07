327ea91d7bcf61ff0dd7bc808a21ab869e321f9f
Chris Stubbs <chris.stubbs@rivermansoftware.co.uk>
2017-12-07 09:40:32 +0000

    Your Order Please

---
+++ CodeWars/CodeWars/YourOrderPlease.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+
+namespace CodeWars
+{
+	public class YourOrderPlease
+	{
+		public static string Order(string words)
+		{
+			if (string.IsNullOrEmpty(words)) return words;
+
+			var wordSplit = words.Split(' ');
+			var wordArray = new string[wordSplit.Length];
+
+			foreach (var word in wordSplit)
+			{
+				var position = int.Parse(Regex.Match(word, @"\d+").Value);
+				wordArray[position - 1] = word;
+			}
+
+			return string.Join(" ", wordArray);
+		}
+	}
+
+	[TestFixture]
+	public class YourOrderPleaseTests
+	{
+		[Test, Description("Sample Tests")]
+		public void SampleTest()
+		{
+			Assert.AreEqual("Thi1s is2 3a T4est", YourOrderPlease.Order("is2 Thi1s T4est 3a"));
+			Assert.AreEqual("Fo1r the2 g3ood 4of th5e pe6ople", YourOrderPlease.Order("4of Fo1r pe6ople g3ood th5e the2"));
+			Assert.AreEqual("", YourOrderPlease.Order(""));
+		}
+	}
+}
+
+
